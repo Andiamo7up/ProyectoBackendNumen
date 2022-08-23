e151c6a75b16673e065126ed9da5d51fbd3bea4b
Santiago <santiagoeponce76@gmail.com>
2022-08-23 19:04:04 -0300

    first commit

---
+++ controller/controller.js
@@ -0,0 +1,53 @@
+const { User } = require('../models/users');
+const { House } = require('../models/casas');
+
+const controllers = {
+    myIndex(req, res) {
+        res.render('index', { title: 'Express' });
+    },
+    myUser(req, res) {
+        res.json({
+            name: "Santiago",
+            age: 46,
+          })
+    },
+    newUser: async (req, res) => {
+        try {
+            const user = new User(req.body);
+            await user.save();
+            res.status(201).json(user);
+        } catch (err) {
+            res.status(501).json({
+                msg: "No se puede guardar el usuario en la DB, ese mail ya existe",
+                err,
+            });
+        }
+    },
+    verUser: async (req, res) => {
+        const users = await User.find();
+        res.json ({ users });
+    },
+};
+
+const vistaCasa = async (req, res) =>{
+    const casas = await House.find()
+    res.json ({casas})
+}
+
+const vistaUnicaCasa = async (req, res) =>{
+    const casas = await House.findById(req, params.id)
+    res.json ({casas})
+}
+
+const crearCasa = async (req, res) =>{
+    try {
+        const save = new House (req.body);
+        await save.save(),
+        res.status(201).json(save)
+    } catch (err) {
+        res.status(501).json({msg: "no se puede guardar la casa, por favor intenta mas tarde", err})
+    }
+}
+
+module.exports = controllers;
+module.exports = {vistaCasa, vistaUnicaCasa, crearCasa}
---
+++ models/casas.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+const storeSchema = new Schema({
+    modelo: {
+        type: String,
+        required: true,
+    },
+    metrosCuadrados: {
+        type: Number,
+        required: true,
+    },
+    habitaciones: {
+        type: Number,
+        required: true,
+    },
+    baños: {
+        type: Number,
+        required: true,
+    },
+    amenities: {
+        type: String,
+    },
+    orientacion: {
+        type: String,
+    },
+    precio: {
+        type: Number,
+        required: true,
+    },
+});
+const House = mongoose.model('House', storeSchema);
+
+module.exports = {House}
